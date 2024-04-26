f8927614005af585411a26b83ff8cbbcf6bf32e9
IamBr4vo <bravo9386@gmail.com>
2024-04-25 18:51:44 -0600

    join the 2 parts of the project into 1 and solve details

--- ticoride_project/app/actions/users/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); //remove all session variables
-session_destroy();
-
-header("Location: /pages/users/login.php"); //redirect the user to the login page
-exit();
-?>
