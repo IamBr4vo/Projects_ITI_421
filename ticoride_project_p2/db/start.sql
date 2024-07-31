ff1e891bb9d3622bf6f48fa08b17088f2e8046c8
IamBr4vo <bravo9386@gmail.com>
2024-07-31 00:03:15 -0600

    Fix sql

--- ticoride_project_p2/db/start.sql
+++ ticoride_project_p2/db/start.sql
@@ -2,6 +2,16 @@ CREATE DATABASE ticoride_project;
 
 USE ticoride_project;
 
+CREATE TABLE IF NOT EXISTS users(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    confirm_password VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS rides (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     user_id INT NOT NULL,
@@ -11,18 +21,8 @@ CREATE TABLE IF NOT EXISTS rides (
     description TEXT NOT NULL,
     departure_time TIME NOT NULL,
     arrival_time TIME NOT NULL,
-    days VARCHAR(255) not NULL
+    days VARCHAR(255) not NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS users(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    confirm_password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE user_settings (
